efee3cdc05b35eac6d489e8131017f0b5f49be27
sonvo0302 <57646470+sonvo0302@users.noreply.github.com>
2019-12-10 09:56:37 +0700

    Initial commit

---
+++ PhoneShop/GoiYSanPham.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage_User.Master" AutoEventWireup="true" CodeBehind="GoiYSanPham.aspx.cs" Inherits="PhoneShop.GoiYSanPham" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="../Mangement_Style/css_table/style.css" rel="stylesheet" />
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="content_top">
+                        <div class="heading">
+                            <h3>Các sản phẩm gợi ý</h3>
+                        </div>
+                        <div class="see">
+    			            <p><a href="#">Tất cả sản phẩm</a></p>
+    		            </div>
+                        <div class="clear"></div>
+                    </div>
+    <asp:DataList ID="dataGoiY" runat="server" CssClass="table table-striped table-advance table-hover">
+        <HeaderTemplate>
+
+            <th><i></i>Tên sản phẩm</th>
+            <th><i></i>Hình ảnh</th>
+            <th><i></i>Đơn giá</th>
+            <th><i></i>Màu</th>
+             <th><i></i>Mô tả</th>
+             
+            <th><i class="glyphicon glyphicon-cog"></i>Lựa chọn</th>
+        </HeaderTemplate>
+        <ItemTemplate>
+
+            <td>
+                <asp:Label runat="server" Text='<%#Eval("TenSanPham") %>'></asp:Label></td>
+            <td>
+                <asp:Image ID="lb1" Style="float: left" Width="50%" runat="server" ImageUrl='<%# "/images/AnhDT/"+Eval("HinhAnh") %>'></asp:Image></td>
+            <td>
+                <asp:Label runat="server" Text='<%#Eval("DonGia") %>'></asp:Label></td>
+            <td>
+                <asp:Label runat="server" Text='<%#Eval("TenMau") %>'></asp:Label></td>
+            <td>
+                <asp:Label runat="server" Text='<%#Eval("MoTa") %>'></asp:Label></td>
+            <td>
+                <div class="btn-group">
+                    <asp:LinkButton ID="btn_mua" runat="server" CommandArgument='<%#Eval("SanPham_ID") %>' CssClass="btn btn-success" OnClick="btn_mua_Click"><i class="glyphicon glyphicon-shopping-cart"></i></asp:LinkButton>              
+                </div>
+            </td>
+
+        </ItemTemplate>
+    </asp:DataList>
+
+
+
+</asp:Content>
---
+++ PhoneShop/ManagementShop/SuaTaikhoan.aspx.cs
@@ -0,0 +1,66 @@
+﻿using PhoneShop.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhoneShop.ManagementShop
+{
+    public partial class SuaTaikhoan : System.Web.UI.Page
+    {
+        COMMON cm = new COMMON();
+        public String strMenu;
+        DataTable dt;
+        string user_id;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            Load_group();
+            user_id = Context.Items["userid"].ToString();
+            string sql = "Select * from tbLOGIN where User_ID='" + user_id + "'";
+            dt = cm.getTable(sql);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Text1.Value = user_id;
+                user_name.Value = row["UserName"].ToString();
+                inputPassword.Value = MaHoaMD5.Decryptdata(row["Password"].ToString());
+                dropdown_group.SelectedItem.Value = row["Group_ID"].ToString();
+            }
+        }
+        private void Load_group()
+        {
+            string sql = "Select * from tbGROUP";
+            dropdown_group.DataValueField = "Group_ID";
+            dropdown_group.DataTextField = "GroupName";
+            dropdown_group.DataSource = cm.getTable(sql);
+            dropdown_group.DataBind();
+        }
+        protected void btn_save_Click(object sender, EventArgs e)
+        {
+
+            string password = MaHoaMD5.Encryptdata(inputPassword.Value.ToString());
+            
+            string sql = "Update tbLOGIN set Group_ID='" + dropdown_group.SelectedItem.Value.ToString() + "' where UserName='" + user_name.Value + "'";
+            cm.ExecuteNonQuery(sql);
+            Server.Transfer("QuanLyTaiKhoan.aspx");
+        }
+
+        protected void btn_back_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("QuanLyTaiKhoan.aspx");
+        }
+        protected void phanquyen_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("PhanQuyen.aspx");
+        }
+
+        protected void qltaikhoan_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("QuanLyTaiKhoan.aspx");
+        }
+    }
+}
